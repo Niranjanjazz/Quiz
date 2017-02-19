763e42491917331e5b334d8bc6004e88c89edd09
ctadmin <davidmessia258@gmail.com>
2017-02-19 12:33:49 +0530

    blah blah blah

--- app/src/main/java/com/example/ctadmin/quiz/MainActivity.java
+++ app/src/main/java/com/example/ctadmin/quiz/MainActivity.java
@@ -3,14 +3,14 @@ package com.example.ctadmin.quiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    int id1;
-    int id2;
-    int id3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,23 +24,47 @@ public class MainActivity extends AppCompatActivity {
                 RadioGroup rg1 = (RadioGroup) findViewById(R.id.rg1);
                 RadioGroup rg2 = (RadioGroup) findViewById(R.id.rg2);
                 RadioGroup rg3 = (RadioGroup) findViewById(R.id.rg3);
-                id1=rg1.getCheckedRadioButtonId();
-                id2=rg2.getCheckedRadioButtonId();
-                id3=rg3.getCheckedRadioButtonId();
+                int id1=rg1.getCheckedRadioButtonId();
+                int id2=rg2.getCheckedRadioButtonId();
+                int id3=rg3.getCheckedRadioButtonId();
                 View rb1 = rg1.findViewById(id1);
                 int s1 = rg1.indexOfChild(rb1);
-                View rb2 = rg1.findViewById(id2);
-                int s2 = rg1.indexOfChild(rb2);
-                View rb3 = rg1.findViewById(id3);
-                int s3 = rg1.indexOfChild(rb3);
+                View rb2 = rg2.findViewById(id2);
+                int s2 = rg2.indexOfChild(rb2);
+                View rb3 = rg3.findViewById(id3);
+                int s3 = rg3.indexOfChild(rb3);
                 if(s1==1)
                     scorecount=scorecount+1;
                 if(s2==2)
                     scorecount=scorecount+1;
                 if(s3==3)
                     scorecount=scorecount+1;
+                CheckBox cb1 = (CheckBox) findViewById(R.id.checkbox1);
+                boolean isChecked1 = cb1.isChecked();
+                CheckBox cb2 = (CheckBox) findViewById(R.id.checkbox2);
+                boolean isChecked2 = cb2.isChecked();
+                CheckBox cb3 = (CheckBox) findViewById(R.id.checkbox3);
+                boolean isChecked3 = cb3.isChecked();
+                CheckBox cb4 = (CheckBox) findViewById(R.id.checkbox4);
+                boolean isChecked4 = cb4.isChecked();
+                if (isChecked1 && isChecked3 && !isChecked2 && !isChecked4) {
+                    scorecount = scorecount + 1;
+                }
+                EditText et=(EditText)findViewById(R.id.EditText);
+                if(et.getText().toString().equals("Katy Perry"))
+                {
+                    scorecount=scorecount+1;
+                }
                 Toast.makeText(MainActivity.this, "Your score is " +scorecount,
-                Toast.LENGTH_SHORT).show();
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        retry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewGroup vg = (ViewGroup) findViewById(R.id.activity_main);
+                vg.invalidate();
+                setContentView(R.layout.activity_main);
             }
         });
     }
